6f5b7daa6fed501d268e04a1d1f0c00a086ec037
Mason Shields <masonlshields@gmail.com>
2022-07-06 21:49:41 -0500

    Tweaked styling, setupp basis for searchbox

--- MetaData/settings.py
+++ MetaData/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "MetaApp",
-    "crispy_forms"
+    "MetaApp"
 ]
 crispy_template_pack = "uni_form"
 MIDDLEWARE = [
