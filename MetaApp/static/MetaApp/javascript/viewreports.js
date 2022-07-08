80c91581aa707cf8db806434a78e40f176507355
Mason Shields <masonlshields@gmail.com>
2022-07-08 00:41:33 -0500

    Tweaked styling and cleaned dead code

--- MetaApp/static/MetaApp/javascript/viewreports.js
+++ MetaApp/static/MetaApp/javascript/viewreports.js
@@ -1,16 +1,10 @@
 searchbox = document.getElementById("searchbox");
 searchbox.addEventListener("focus", OnFocus);
 searchbox.addEventListener("focusout", OnUnfocus);
-searchbox.addEventListener("keyup", SearchFunction);
 function OnFocus(){
     if (searchbox.value == "Search by Report Name..."){
         searchbox.value = "";
         searchbox.style.color = "#0B2545"
-    }
-}
-function SearchFunction(event){
-    if (searchbox.value != "Search by Report Name..." && searchbox.value != "" && event.key === "Enter"){
-        reportslistjs.fuzzySearch(searchbox.value);
     }
 }
 function OnUnfocus(){
