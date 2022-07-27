179843d6084d3ae6fc64f16f3e85b88fc59f7a3b
Mason <masonlshields@gmail.com>
2022-07-27 11:16:52 -0500

    Test Commit

--- MetaData/settings.py
+++ MetaData/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "MetaApp"
+    "MetaApp",
+    "whitenoise.runserver_nostatic"
 ]
 crispy_template_pack = "uni_form"
 MIDDLEWARE = [
